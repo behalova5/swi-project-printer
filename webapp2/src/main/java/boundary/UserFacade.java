9c6191d29513bf67b6466a0f7da997b296c5029c
Zdeněk Braun <>
2015-03-25 10:57:14 +0100

    Opraveny UserFacade od databazistu, namisto metody insertUser(User user), existuje již metoda create(T Entity). Tak stejně pro getUsersList() existuje metoda findAll(), která vám vrací List<User>. A nakonec metoda changeRole(int role), ve fasade vubec nebude. To je cast aplikacni logiky. Cili je zde metoda edit(T Entity). Funguje to tak, ze pokud chcete zmenit roli, tak si uzivatele z databaze vytahnete, zmenite mu roli (ne v UserFacade, ale nekde jinde) a pak novyho usera zas vratite do DB pomoci metody edit(T Entity).

--- webapp2/src/main/java/boundary/UserFacade.java
+++ webapp2/src/main/java/boundary/UserFacade.java
@@ -1,13 +1,9 @@
 package boundary;
 
-import managebeans.LogForm;
-import managebeans.RegForm;
 import entities.User;
-import java.util.ArrayList;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
 
 /**
  *
@@ -29,40 +25,16 @@ public class UserFacade extends AbstractFacade<User>
     {
             super(User.class);
     }
-    public ArrayList<User> getAllUsers()
-    {
-        Query query = em.createQuery("SELECT u FROM User u");
-        return (ArrayList<User>) query.getResultList();
-    }
+    
     public User getUserByLogin(String login)
     {
-        //TODO, now returning user with id 1
-        return em.find(User.class, 1);
-    }
-    public boolean insertUser(RegForm newUser)
-    {
-        User u = new User();
-        u.setLogin(newUser.getLogin());
-        u.setPassHash(newUser.getPass());
-        u.setEmail(newUser.getEmail());
-        u.setRole(newUser.getRole());
-        u.setSurname(newUser.getSurname());
-        u.setName(newUser.getName());
-        if(!getAllUsers().contains(u)){
-            em.persist(u);
-            return true;
-        }
-        return false;
-    }
-    public boolean changeRole(User user, int role)
-    {
-        return false;
+        return (User) em.createQuery("SELECT * FROM User u WHERE login=:userLogin")
+                .setParameter("userLogin", login)
+                .getSingleResult();
     }
     
-    public boolean existsUser(LogForm user) {
-        User u = new User();
-        u.setLogin(user.getLogin());
-        return getAllUsers().contains(u);
+    public boolean existsUser(User user) {
+        return findAll().contains(user);
     }
         
 	
